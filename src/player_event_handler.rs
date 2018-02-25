dd47d482eb06d6c55ad2d4bbc83d2b77a7cfb731
Sasha Hilton <sashahilton00@gmail.com>
2018-02-25 01:23:49 +0100

    Format according to rustfmt guidelines

--- src/player_event_handler.rs
+++ src/player_event_handler.rs
@@ -15,16 +15,19 @@ fn run_program(program: &str, env_vars: HashMap<&str, String>) {
 pub fn run_program_on_events(event: PlayerEvent, onevent: &str) {
     let mut env_vars = HashMap::new();
     match event {
-        PlayerEvent::Changed { old_track_id, new_track_id } => {
+        PlayerEvent::Changed {
+            old_track_id,
+            new_track_id,
+        } => {
             env_vars.insert("PLAYER_EVENT", "change".to_string());
             env_vars.insert("OLD_TRACK_ID", old_track_id.to_base16());
             env_vars.insert("TRACK_ID", new_track_id.to_base16());
-        },
+        }
         PlayerEvent::Started { track_id } => {
             env_vars.insert("PLAYER_EVENT", "start".to_string());
             env_vars.insert("TRACK_ID", track_id.to_base16());
         }
-        PlayerEvent::Stopped { track_id } =>  {
+        PlayerEvent::Stopped { track_id } => {
             env_vars.insert("PLAYER_EVENT", "stop".to_string());
             env_vars.insert("TRACK_ID", track_id.to_base16());
         }
